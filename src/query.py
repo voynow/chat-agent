c000a4821d81ed67761f0399f2e0d75b9bc47e14
voynow <voynow99@gmail.com>
2024-02-12 07:04:50 -0500

    Summary query func added

--- src/query.py
+++ src/query.py
@@ -1,8 +1,10 @@
+import fitz
 from llama_index import (
     SimpleDirectoryReader,
     VectorStoreIndex,
 )
 from llama_index.core.base_query_engine import BaseQueryEngine
+from llama_index.llms import OpenAI
 
 
 def rag_query(input_dir, query) -> BaseQueryEngine:
@@ -13,8 +15,17 @@ def rag_query(input_dir, query) -> BaseQueryEngine:
     return index.as_query_engine().query(query)
 
 
-response = rag_query(
-    "data/autogen", "What is conversation programming and how does autogen use it?"
+def summarization_query(pdf_path, query) -> str:
+    """Send entire document to query engine for summarization."""
+    text = ""
+    with fitz.open(pdf_path) as doc:
+        for page in doc:
+            text += page.get_text()
+    return OpenAI().complete(query)
+
+
+response = summarization_query(
+    "data/autogen/AutoGen.pdf", "Summarize the autogen paper."
 )
 
 print(response)
